9e44f0bb6c91e2baa82675e036e64a6b9681c200
Proggit <proggit@mail.ru>
2021-12-11 22:53:53 +0500

    step 3

---
+++ tests/Http/Controllers/MainPageControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\MainPageController;
+use Tests\TestCase;;
+
+class MainPageControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $response = $this->get(route('main'));
+        $response->assertOk();
+    }
+}
---
+++ tests/Http/Controllers/UrlControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class UrlControllerTest extends TestCase
+{
+    private $id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $generatedHostName = 'http://' . strtolower(Str::random(5)) . ".ru";
+        $this->id = DB::table('urls')->insertGetId([
+            'name' => $generatedHostName,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('urls.index'));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $generatedHostName = 'http://' . strtolower(Str::random(5)) . ".ru";
+        $urlData = ['url' => ['name' => $generatedHostName]];
+        $response = $this->post(route('urls.store'), $urlData);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('urls', $urlData['url']);
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('urls.index', ['id' => $this->id]));
+        $response->assertOk();
+    }
+}
